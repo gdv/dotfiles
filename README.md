ccfca41169da0eb217aff0a06e5c0ee1c8bbf1db
Gianluca Della Vedova <gianluca@dellavedova.org>
2018-10-09 09:00:49 +0200

    Simpler deploy

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 To initialize a computer run:
 
 ```
-git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-git clone https://github.com/Bash-it/bash-it.git ~/.bash_it
 git clone https://github.com/gdv/dotfiles.git
+dotfiles/deploy.sh
 ```
---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+test -e ~/.emacs.d && rm -fr ~/.emacs.d
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+test -e ~/.bash_it && rm -fr ~/.bash_it
+git clone https://github.com/Bash-it/bash-it.git ~/.bash_it
+
+dir=$(dirname "$0")
+for f in      .bash_login \
+             .bash_logout \
+             .bash_profile \
+             .bashrc \
+             .spacemacs.d
+do
+    test -e ~/"$f" && rm -fr ~/"$f"
+    ln -s "$dir"/"$f" ~/"$f"
+done
