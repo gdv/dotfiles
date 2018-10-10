e80b65ffd8a0eecc469141b0672e4ae01f4e068b
Gianluca Della Vedova <gianluca@dellavedova.org>
2018-10-10 18:34:55 +0200

    Don't indent latex

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -661,6 +661,7 @@ you should place your code here."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(LaTeX-indent-level 0)
  '(abbrev-file-name "~/.spacemacs.d/abbrev_defs.el")
  '(aggressive-indent-excluded-modes
    (quote
