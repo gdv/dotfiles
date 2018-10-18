e4c41105bb7d73439734beeb1dc8731e9f042a04
Gianluca Della Vedova <gianluca@dellavedova.org>
2018-10-18 19:47:40 +0200

    Get vim plugins

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ test -e ~/.emacs.d && rm -fr ~/.emacs.d
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 test -e ~/.bash_it && rm -fr ~/.bash_it
 git clone https://github.com/Bash-it/bash-it.git ~/.bash_it
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
 
 dir=$(dirname "$0")
 for f in      .bash_login \
