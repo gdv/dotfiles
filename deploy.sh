71a7ffac69eb038d39d44e537ad3b1813e53dda3
Gianluca Della Vedova <gianluca@dellavedova.org>
2018-12-26 10:19:51 +0100

    run deploy whenever you like

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-test -e ~/.emacs.d && rm -fr ~/.emacs.d
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-test -e ~/.bash_it && rm -fr ~/.bash_it
 git clone https://github.com/Bash-it/bash-it.git ~/.bash_it
+mkdir -p ~/.vim/bundle/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
@@ -23,4 +22,4 @@ do
 done
 
 mkdir -p ~/.vim/spell
-cd ~/.vim/spell/ && wget http://ftp.vim.org/vim/runtime/spell/it.utf-8.spl
+cd ~/.vim/spell/ && test -f it.utf-8.spl || wget http://ftp.vim.org/vim/runtime/spell/it.utf-8.spl
